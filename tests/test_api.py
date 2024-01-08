47669fdceaa9b4c8b74f0dc4e68ce43ccc47f15b
sushi <yihsiang@barkingdog.ai>
2024-01-08 18:59:31 +0800

    update17

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,16 @@
 import json
 
 import pytest
+from functools import (
+    partial,
+    wraps,
+    update_wrapper,
+    WRAPPER_ASSIGNMENTS,
+    WRAPPER_UPDATES,
+    WRAPPER_ASSIGNMENTS,
+    WRAPPER_UPDATES,
+)
+
 import requests
 
 
@@ -9,7 +19,7 @@ def test_ex1():
 
 
 def test_ex2():
-    assert 1 == 1 
+    assert 1 == 1
 
 
 def test_ex3():
@@ -34,8 +44,8 @@ def test_context_chain_api():
             "llm_name": "gpt-3.5-turbo-1106-openai",
             "timeout": -1,
             "max_retries": 2,
-            "temperature": 0
-        }
+            "temperature": 0,
+        },
     }
     response = requests.post(api_url, data=json.dumps(data))
     assert response.status_code == 200
@@ -52,8 +62,8 @@ def test_chain_api():
             "llm_name": "gpt-3.5-turbo-1106-openai",
             "timeout": 20,
             "max_retries": 2,
-            "temperature": 0
-        }
+            "temperature": 0,
+        },
     }
     response = requests.post(api_url, data=json.dumps(data))
     assert response.status_code == 200
